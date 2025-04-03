0fd0edeb1f721aaf345a2ed66a58a4f83bc8ad03
aditya dey <adiitya.dey@gmail.com>
2025-04-03 11:50:27 +0200

    first commit.

---
+++ scripts/univariate/HADL/traffic.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+model_name=HADL
+
+root_path_name=./dataset/
+data_path_name=traffic.csv
+model_id_name=traffic
+data_name=custom
+
+rank=50
+
+for seq_len in 512
+do
+for pred_len in 96 192 336 720
+do
+  python -u run_longExp.py \
+    --is_training 1 \
+    --individual 0 \
+    --root_path $root_path_name \
+    --data_path $data_path_name \
+    --model_id $model_id_name'_'$seq_len'_'$pred_len \
+    --model $model_name \
+    --data $data_name \
+    --features S \
+    --train_type Linear \
+    --seq_len $seq_len \
+    --pred_len $pred_len \
+    --rank $rank \
+    --enc_in 862 \
+    --train_epochs 50 \
+    --rank $rank \
+    --bias 1 \
+    --enable_Haar 1 \
+    --enable_DCT 1 \
+    --enable_lowrank 1 \
+    --enable_iDCT 0 \
+    --patience 10 \
+    --des 'Exp' \
+    --regularizer 1 \
+    --regularization_rate 0.1 \
+    --itr 1 \
+    --batch_size 32 \
+    --learning_rate 0.01
+done
+done
+
