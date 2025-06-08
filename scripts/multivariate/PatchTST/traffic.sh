f5d0648724a34e383d1239fd85704bfef54e9ba1
aditya dey <adiitya.dey@gmail.com>
2025-06-08 09:18:53 +0200

    multivariate scripts added.

---
+++ scripts/multivariate/PatchTST/traffic.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+model_name=PatchTST
+
+root_path_name=./dataset/
+data_path_name=traffic.csv
+model_id_name=traffic
+data_name=custom
+
+for seq_len in 336 512 720
+do
+for pred_len in 24 48 96 192 336 512 720
+do    
+    python -u run_longExp.py \
+      --is_training 1 \
+      --individual 1 \
+      --root_path $root_path_name \
+      --data_path $data_path_name \
+      --model_id $model_id_name_$seq_len'_'$pred_len \
+      --model $model_name \
+      --data $data_name \
+      --features S \
+      --train_type Linear \
+      --seq_len $seq_len \
+      --pred_len $pred_len \
+      --enc_in 1 \
+      --e_layers 3 \
+      --n_heads 16 \
+      --d_model 128 \
+      --d_ff 256 \
+      --dropout 0.2\
+      --fc_dropout 0.2\
+      --head_dropout 0\
+      --patch_len 16\
+      --stride 8\
+      --des 'Exp' \
+      --train_epochs 50 \
+      --patience 10 \
+      --itr 1 --batch_size 32 --learning_rate 0.0001 
+done
+done
+
