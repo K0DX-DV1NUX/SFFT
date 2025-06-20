4e87441cb9197b1225b953ba1928ba9a53a07314
aditya dey <adiitya.dey@gmail.com>
2025-06-20 19:44:22 +0200

    PatchTST Multivariate scripts ready.

--- scripts/multivariate/PatchTST/etth2.sh
+++ scripts/multivariate/PatchTST/etth2.sh
@@ -11,10 +11,10 @@ data_path_name=ETTh2.csv
 model_id_name=ETTh2
 data_name=ETTh2
 
+for pred_len in 96 192 336 720
+do  
 for seq_len in 336 512 720
-do
-for pred_len in 24 48 96 192 336 512 720
-do    
+do  
     python -u run_longExp.py \
       --is_training 1 \
       --individual 1 \
@@ -23,11 +23,11 @@ do
       --model_id $model_id_name_$seq_len'_'$pred_len \
       --model $model_name \
       --data $data_name \
-      --features S \
+      --features M \
       --train_type Linear \
       --seq_len $seq_len \
       --pred_len $pred_len \
-      --enc_in 1 \
+      --enc_in 7 \
       --e_layers 3 \
       --n_heads 4 \
       --d_model 16 \
@@ -38,8 +38,8 @@ do
       --patch_len 16\
       --stride 8\
       --des 'Exp' \
-      --train_epochs 50\
-      --patience 10 \
+      --train_epochs 100\
+      --patience 20 \
       --itr 1 --batch_size 32 --learning_rate 0.0001 
 done
 done
